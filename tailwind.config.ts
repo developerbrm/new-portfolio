f4c2d7035011665588522d842140510bd79f9af4
developerbrm <developerbrm@gmail.com>
2025-02-04 14:41:44 +0530

    - the config and sections updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ export default {
     './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './hooks/**/*.{js,ts,jsx,tsx,mdx}',
 
     // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx,mdx}',
